8d8f1086979dccbfba1a60832053ea8a46881526
yashverma2110 <yash.silver2110@gmail.com>
2020-08-22 16:09:41 +0530

    Updates

---
+++ precache-manifest.5948567f078236f9488092ff072ff334.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0d8679c63ee28a8e298fe4c465ade4",
+    "url": "/IPL-stats/index.html"
+  },
+  {
+    "revision": "da5a884074ebe95b770d",
+    "url": "/IPL-stats/static/css/3.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ae55a2452b2cf47acafc",
+    "url": "/IPL-stats/static/css/main.7d4bbb38.chunk.css"
+  },
+  {
+    "revision": "5550b283e824485400c7",
+    "url": "/IPL-stats/static/js/2.9755516e.chunk.js"
+  },
+  {
+    "revision": "8c4a52c548d60367d74557f7ce242617",
+    "url": "/IPL-stats/static/js/2.9755516e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5a884074ebe95b770d",
+    "url": "/IPL-stats/static/js/3.96772e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IPL-stats/static/js/3.96772e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba92023c14ff1611a7c3",
+    "url": "/IPL-stats/static/js/4.c0ddcf62.chunk.js"
+  },
+  {
+    "revision": "ae55a2452b2cf47acafc",
+    "url": "/IPL-stats/static/js/main.0c386c50.chunk.js"
+  },
+  {
+    "revision": "3d0833a1340f7e2e93e5",
+    "url": "/IPL-stats/static/js/runtime-main.831ff3b0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IPL-stats/precache-manifest.5948567f078236f9488092ff072ff334.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IPL-stats/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
